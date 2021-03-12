6e61c0b98210d940b2919cf0f82a78c583901683
None <zanoni.reibnitz@gmail.com>
2021-03-11 21:33:24 -0300

    2_1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# DataIngestionWithPython
+# Data Ingestion With Python
 [Course](https://www.linkedin.com/learning/data-ingestion-with-python?contextUrn=urn%3Ali%3AlyndaLearningPath%3A5b61ea25498e580437e51859&amp;u=26137906)
+
+#Notes
+- ETL (Extract, Transform and Load)
---
+++ csv_reading/_csv.py
@@ -0,0 +1,47 @@
+import bz2
+import csv
+from collections import namedtuple
+from datetime import datetime
+
+# bz2 is a file decompressor
+# csv is used to read csv files
+
+Column = namedtuple('Column', 'src dest convert')
+
+
+def parse_timestamp(text):
+    return datetime.strptime(text, '%Y-%m-%d %H:%M:%S')
+
+
+columns = [
+    Column('VendorID', 'vendor_id', int),
+    Column('passenger_count', 'num_passengers', int),
+    Column('tip_amount', 'tip', float),
+    Column('total_amount', 'price', float),
+    Column('tpep_dropoff_datetime', 'dropoff_time', parse_timestamp),
+    Column('tpep_pickup_datetime', 'pickup_time', parse_timestamp),
+    Column('trip_distance', 'distance', float),
+]
+
+
+def iter_records(file_name):
+    with bz2.open(file_name, 'rt') as fp:
+        reader = csv.DictReader(fp)
+        for csv_record in reader:
+            record = {}
+            for col in columns:
+                value = csv_record[col.src]
+                record[col.dest] = col.convert(value)
+            yield record
+
+
+def example():
+    from pprint import pprint
+
+    for i, record in enumerate(iter_records('taxi.csv.bz2')):
+        if i >= 10:
+            break
+        pprint(record)
+
+
+example()
